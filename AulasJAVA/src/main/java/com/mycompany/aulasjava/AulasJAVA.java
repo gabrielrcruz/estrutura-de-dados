9457121d0107b6dd014d32fd6ec58b40df266a33
Gabriel Ribeiro da Cruz <56709233+gabrielrcruz@users.noreply.github.com>
2026-03-23 10:58:21 -0300

    Add files via upload

---
+++ AulasJAVA/src/main/java/com/mycompany/aulasjava/AulasJAVA.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aulasjava;
+
+/**
+ *
+ * @author Alunos
+ */
+public class AulasJAVA {
+
+    public static void main(String[] args) {
+        int A = 5;
+        float salario = 1500.33f;
+        int J = 3;
+        System.out.println(5 + 3 * 2 - 6 / 4);
+        System.out.println((5 + 3) * 2 - 6 / 4);
+        System.out.println((5 + 3) * (2 - 6 / 4));
+        System.out.println((5 + 3 * 2) - 6 / 4);
+        System.out.println(((5 + 3 * 2) - 6) / 4);
+        System.out.println((float) J / A * 2 - A);
+        System.out.println(Math.sqrt (21 + 4) * 2 / 4);
+        System.out.println(20 / J / J + Math.pow (A, J));
+        System.out.println(Math.pow (Math.sqrt (-1 + A * 2 ), J));
+        System.out.println(salario + salario / 100 * (A * J));
+    }
+}
